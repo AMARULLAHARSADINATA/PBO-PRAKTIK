b1e8d7736147da9df9e5ae341b0945712a1d54cb
AMARULLAHARSADINATA <arsadinata2424@gmail.com>
2024-09-22 14:29:11 +0700

    Add files via upload

---
+++ bangundatar.py
@@ -0,0 +1,17 @@
+# PEMROGRAMAN BERORIENTASI OBJEK PRAKTIK
+
+# NAMA : AMARULLAH ARSADINATA
+# NPM  : 5230411323
+
+# RUMUS LUAS PERMUKAAN TABUNG
+
+pi = 3.14
+print("masukan dalam bentuk angka :))")
+print("===========================")
+r = int(input("masukan jari-jari :"))
+t = int(input("masukan tinggi :"))
+kurung = r + t
+rumus = 2*pi*r*kurung
+print("===========================")
+print("Hasil yag didapat adalah :")
+print(rumus)
